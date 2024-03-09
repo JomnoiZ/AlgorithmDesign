914d8006d772290c1380998e089622b87e6ff224
Kamanun Maneesri <jomlovejen7956@gmail.com>
2024-03-09 15:43:03 +0700

    edit: bottom up mcm

--- ex03e2_​mcm.cpp
+++ ex03e2_​mcm.cpp
@@ -7,17 +7,6 @@ const long long INF = 1e18 + 7;
 long long a[MAX_N];
 long long dp[MAX_N][MAX_N];
 
-long long solve(int l, int r) {
-    if (dp[l][r] != -1) return dp[l][r];
-    if (l == r) return dp[l][r] = 0;
-
-    dp[l][r] = INF;
-    for (int m = l; m < r; m++) {
-        dp[l][r] = min(dp[l][r], solve(l, m) + solve(m + 1, r) + a[l] * a[m + 1] * a[r + 1]);
-    }
-    return dp[l][r];
-}
-
 int main() {
     cin.tie(nullptr)->sync_with_stdio(false);
 
@@ -26,8 +15,14 @@ int main() {
 
     for (int i = 1; i <= n + 1; i++) cin >> a[i];
 
-    memset(dp, -1, sizeof(dp));
-    
-    cout << solve(1, n);
+    for (int len = 2; len <= n; len++) {
+        for (int l = 1, r = len; r <= n; l++, r++) {
+            dp[l][r] = INF;
+            for (int m = l; m < r; m++) {
+                dp[l][r] = min(dp[l][r], dp[l][m] + dp[m + 1][r] + a[l] * a[m + 1] * a[r + 1]);
+            }
+        }
+    }
+    cout << dp[1][n];
     return 0;
 }
